244d220b5daeecb50dcf263bcee4f11020955fbe
dsullivan7 <dbsullivan23@gmail.com>
2016-01-03 18:37:16 +0100

    adding some styling, like buttons

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,36 @@
+/*
+Renders the photo for this person
+*/
 var PhotoView = React.createClass({
     render: function() {
-        return (<img src={"img/img1.jpg"} />)
+        return (<img className="flix_display_image" src={"img/img1.jpg"} />)
     }
 })
 
+/*
+A left or right arrow to indicate whether to like this person
+*/
+var Arrow = React.createClass({
+    render: function() {
+        if (this.props.option === "like"){
+            return (<button className="flix_arrow flix_arrow_left glyphicon glyphicon-thumbs-down"></button>)
+        }else{
+            return (<button className="flix_arrow flix_arrow_right glyphicon glyphicon-thumbs-up"></button>)
+        }
+    }
+})
+
+/*
+Main content container for the app
+*/
 var AppContainer = React.createClass({
     render: function() {
-        return (<PhotoView />)
+        return (
+            <div id="flix_app_content">
+                <Arrow option="like"/>
+                <PhotoView />
+                <Arrow option="dislike"/>
+            </div>)
     }
 });
 
