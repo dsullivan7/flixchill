0bc75022be887d6ac0923818308842e3b00ed04f
dsullivan7 <dbsullivan23@gmail.com>
2016-01-03 15:14:16 +0100

    displaying test image

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,16 @@
+var PhotoView = React.createClass({
+    render: function() {
+        return (<img src={"img/img1.jpg"} />)
+    }
+})
+
+var AppContainer = React.createClass({
+    render: function() {
+        return (<PhotoView />)
+    }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
+    <AppContainer />,
+    document.getElementById('container')
 );
