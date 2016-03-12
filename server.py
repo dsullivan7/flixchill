96d7475eb40316887d2d5535e4ebc132011c110d
dsullivan7 <dbsullivan23@gmail.com>
2016-03-12 15:11:39 +0100

    adding Procfile and gunicorn

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-import os
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__, static_url_path='')
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 @app.route('/')
@@ -9,4 +10,5 @@ def root():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 3000)))
+    # app.run(port=int(os.environ.get("PORT", 3000)))
+    app.run()
