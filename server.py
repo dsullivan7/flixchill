7a136f09b85c69946b203148e972a3b7ec3937c4
dsullivan7 <dbsullivan23@gmail.com>
2016-03-12 15:19:45 +0100

    rearaging lines in server file

--- server.py
+++ server.py
@@ -2,12 +2,13 @@ from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__, static_url_path='')
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     # app.run(port=int(os.environ.get("PORT", 3000)))
